3eb46038fc71a142a281d130822ce02fc6deade6
Atif Ali <atifshoukatali@yahoo.com>
2025-08-04 15:18:51 -0700

    add db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
