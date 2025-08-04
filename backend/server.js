8fef0aa3b709e0609d02285efddebe7c4fcb3b6c
Atif Ali <atifshoukatali@yahoo.com>
2025-08-04 14:24:12 -0700

    pull out routes in a file

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,10 @@
 import express from "express"
+import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express();
 const port = 5001;
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("You have 5 notes");
-});
-
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({ message: "Note created successfully!" });
-});
-
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note updated successfully!" });
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({ message: "Note delete successfully!" });
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
