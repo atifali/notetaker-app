fcdb7e647bd9709554792479e0bc6104c7d5ff26
Atif Ali <atifshoukatali@yahoo.com>
2025-08-04 11:16:43 -0700

    add nodemon package

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from "express"
 
 const app = express();
 const port = 5001;
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 5 notes");
+});
+
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
 });
