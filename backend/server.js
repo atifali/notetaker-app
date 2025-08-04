42f9327d69eccb2a5679744fcae1f2dd156d3294
Atif Ali <atifshoukatali@yahoo.com>
2025-08-04 14:09:28 -0700

    add api routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 5001;
 
 app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got 5 notes");
+    res.status(200).send("You have 5 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({ message: "Note created successfully!" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note updated successfully!" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note delete successfully!" });
 });
 
 app.listen(port, () => {
