ba5c70b0374bb08d090ae22fc73dd0c790707881
Atif Ali <atifshoukatali@yahoo.com>
2025-08-08 20:42:57 -0700

    add rateLimiter logic

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,5 +1,18 @@
-const rateLimiter = async (req, res, next) => {
+import ratelimit from "../config/upstash";
 
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await ratelimit.limit("limit-key");
+        if (!success) {
+            return res.status(429).json({
+                message: "Too many requests, please try again later!"
+            });
+        }
+        next();
+    } catch (error) {
+        console.error("Error in rateLimiter", error);
+        next(error);
+    }
 }
 
 export default rateLimiter;
