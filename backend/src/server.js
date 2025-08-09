dbe288fa95a06a8e1f233164d918fcea398a00f6
Atif Ali <atifshoukatali@yahoo.com>
2025-08-09 11:14:33 -0700

    add logging to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,14 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+import morgan from "morgan"
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
