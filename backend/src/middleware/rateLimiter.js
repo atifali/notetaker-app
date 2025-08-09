3986bc8605e00142d0979ec6cb303823c30923a4
Atif Ali <atifshoukatali@yahoo.com>
2025-08-08 21:07:13 -0700

    use ip in redis ratelimit key

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
     try {
-        const { success } = await ratelimit.limit("limit-key");
+        const { success } = await ratelimit.limit(`rate-limit:${req.ip}`);
         if (!success) {
             return res.status(429).json({
                 message: "Too many requests, please try again later!"
