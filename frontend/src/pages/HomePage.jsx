6e2354f80fb7ad448ea0ed686256152e78ab0697
Atif Ali <atifshoukatali@yahoo.com>
2025-08-14 18:07:38 -0700

    add rate limit banner

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
 import Navbar from "../components/Navbar"
+import RateLimitBanner from "../components/RateLimitBanner";
 
 const HomePage = () => {
+    const [rateLimited, setRateLimited] = useState(false);
     return (
         <div className="min-h-screen">
             <Navbar />
+            {rateLimited && <RateLimitBanner />}
         </div>
     )
 }
