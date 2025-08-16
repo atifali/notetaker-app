74dbde26a11b92704dbf55bc729fa5cc1f48667b
Atif Ali <atifshoukatali@yahoo.com>
2025-08-16 14:09:18 -0700

    add ratelimit logic to fetch notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
 import RateLimitBanner from "../components/RateLimitBanner"
 import axios from "axios"
+import toast from "react-hot-toast"
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(false);
@@ -13,8 +14,17 @@ const HomePage = () => {
             try {
                 const res = await axios.get("http://localhost:5001/api/notes");
                 console.log(res.data);
+                setNotes(res.data);
+                setIsRateLimited(false);
             } catch (error) {
                 console.error("Error fetching notes", error);
+                if (error.response.status === 429) {
+                    setIsRateLimited(true);
+                } else {
+                    toast.error("Failed to load Notes!");
+                }
+            } finally {
+                setLoading(false);
             }
         }
 
