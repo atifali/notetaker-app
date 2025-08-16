ca17cc73ca0016cfe89bfdd4519f0c424c0db804
Atif Ali <atifshoukatali@yahoo.com>
2025-08-15 17:00:52 -0700

    add fetch notes logic

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
-import RateLimitBanner from "../components/RateLimitBanner";
+import RateLimitBanner from "../components/RateLimitBanner"
+import axios from "axios"
 
 const HomePage = () => {
     const [isRateLimited, setIsRateLimited] = useState(false);
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                const res = await axios.get("http://localhost:5001/api/notes");
+                console.log(res.data);
+            } catch (error) {
+                console.error("Error fetching notes", error);
+            }
+        }
+
+        fetchNotes();
+    }, []);
 
     return (
         <div className="min-h-screen">
