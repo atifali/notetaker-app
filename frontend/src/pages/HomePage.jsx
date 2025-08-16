887b6e9bcf5ce289538a33b89bb0a494a1ab8001
Atif Ali <atifshoukatali@yahoo.com>
2025-08-16 14:21:43 -0700

    show notes on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,6 +35,22 @@ const HomePage = () => {
         <div className="min-h-screen">
             <Navbar />
             {isRateLimited && <RateLimitBanner />}
+            <div className="max-w-7xl mx-auto p-4 mt-6">
+                {loading &&
+                    <div className="text-center text-primary py-10">
+                        Loading Notes...
+                    </div>
+                }
+                {notes.length > 0 && !isRateLimited &&
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {notes.map(note => (
+                            <div key={note._id}>
+                                {note.title} | {note.content}
+                            </div>
+                        ))}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
