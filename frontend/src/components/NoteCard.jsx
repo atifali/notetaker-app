e386bff87f132c2f2442f0722d0d25be22b9458e
Atif Ali <atifshoukatali@yahoo.com>
2025-08-17 11:07:11 -0700

    add note title to card

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router"
+
 const NoteCard = ({ note }) => {
     return (
-        <div>{note.title} | {note.content}</div>
+        <Link to={`/note/${note._id}`}
+            className="card bg-base-100 hover:shadow-lg transition-all duration-200
+                border-t-4 border-solid border-primary"
+        >
+            <div className="card-body">
+                <h3>{note.title}</h3>
+            </div>
+        </Link>
     )
 }
 
