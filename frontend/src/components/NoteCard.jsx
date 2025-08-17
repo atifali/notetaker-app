53f28a90ed4c89c066554936a1cc0d70f036a262
Atif Ali <atifshoukatali@yahoo.com>
2025-08-17 11:09:52 -0700

    add content to NoteCard

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router"
 const NoteCard = ({ note }) => {
     return (
         <Link to={`/note/${note._id}`}
-            className="card bg-base-100 hover:shadow-lg transition-all duration-200
+            className="card bg-base-200 hover:shadow-lg transition-all duration-200
                 border-t-4 border-solid border-primary"
         >
             <div className="card-body">
-                <h3>{note.title}</h3>
+                <h3 className="card-title text-base-content">{note.title}</h3>
+                <p className="text-base-content/70 line-clamp-3">{note.content}</p>
             </div>
         </Link>
     )
