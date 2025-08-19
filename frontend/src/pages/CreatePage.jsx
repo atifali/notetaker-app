8fa747bb9a63bdf188f3f5a925569a3e74c719a2
Atif Ali <atifshoukatali@yahoo.com>
2025-08-18 17:28:51 -0700

    create a back link on createNote page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,6 +1,27 @@
+import { ArrowLeftIcon } from "lucide-react";
+import { useState } from "react"
+import { Link } from "react-router";
+
 const CreatePage = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>CreatePage</div>
+        <div className="min-h-screen bg-base-200">
+            <div className="container mx-auto px-4 py-8">
+                <div className="max-w-2xl mx-auto">
+                    <Link to={"/"} className="btn btn-ghost mb-6">
+                        <ArrowLeftIcon className="size-5" />
+                        Back to Notes
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 
