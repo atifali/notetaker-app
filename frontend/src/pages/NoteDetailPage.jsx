cee8b8eff76f71fd18d6c8a24ceee63d4c7bed30
Atif Ali <atifshoukatali@yahoo.com>
2025-08-26 19:32:19 -0700

    handle fetching the note

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router";
+import api from "../lib/axios"
+import toast from "react-hot-toast";
 
 const NoteDetailPage = () => {
     const [note, setNote] = useState(null);
@@ -7,6 +9,27 @@ const NoteDetailPage = () => {
     const [saving, setSaving] = useState(false);
     const navigate = useNavigate();
     const { id } = useParams();
+
+    useEffect(() => {
+        const fetchNote = async () => {
+            try {
+                const res = await api.get(`/notes/${id}`);
+                setNote(res.data);
+            } catch (error) {
+                if (error.response.status === 429) {
+                    toast.error("Slow down! You are fetching note too quickly!", {
+                        duration: 4000,
+                        icon: "💀"
+                    });
+                } else {
+                    toast.error("Failed to fetch the Note!");
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchNote();
+    }, [id]);
 
     return (
         <div>NoteDetailPage</div>
